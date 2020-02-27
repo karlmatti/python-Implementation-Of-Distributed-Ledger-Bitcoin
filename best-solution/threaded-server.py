89b1678f1c3c64b62ae3500c4ce355f4be4a4128
razilj <Rugam.rasmus@gmail.com>
2020-02-27 15:37:53 +0200

    2 TODOS

--- best-solution/threaded-server.py
+++ best-solution/threaded-server.py
@@ -37,27 +37,41 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 def client(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-
-
     try:
+
+        sock.connect((ip, port))
         request = "GET /getpeers HTTP/1.1\r\nHost:%s\r\nUser-agent: client_4\r\n" % ip
         sock.send(request.encode())
         print(request)
         # receive some data
 
         response = sock.recv(4096)
-        if len(response) < 1:
-            # TODO: kustuta peer ära, sest vastus on 404
-            pass
+        #if len(response) < 1:
+
         http_response = repr(response)
         http_response_len = len(http_response)
-
         # display the response
         print("[RECV] - length: %d" % http_response_len)
         print(http_response)
-
         # TODO: vaata mis peerid on olemas ja lisa puudu olevad
+
+    except socket.error as e:
+        print("Hallo hallo, läksin perse")
+        # TODO: kustuta peer ära, sest vastus on 404
+        with open('peers-' + sys.argv[1] + '.json', 'r+') as f:
+
+            response_body = json.load(f)
+            print(response_body['peers'])
+
+            server_address = str(ip) + ':' + str(port)
+            print('server_address on ' + server_address)
+            if server_address in response_body['peers']:
+
+                deleted_peer_index = peer.index(server_address)  # See IP peab olema muutujana
+                response_body['peers'].pop(deleted_peer_index)
+
+                json_data = json.dumps(response_body)
+                json.dump(json_data,f)
 
     finally:
         sock.close()
@@ -81,12 +95,24 @@ if __name__ == "__main__":
     server_thread.start()
     print("Server loop running in thread:", server_thread.name)
 
+    # Reads default peers from peers-default.json file
+    with open('peers-default.json', 'r') as f:
+        data = json.load(f)
+
+    # Creates or overwrites peers-<PORT>.json file with default peers
+    f = open('peers-' + sys.argv[1] + '.json', "w+")
+    f.write(json.dumps(data))
+    f.close()
+
     prev = time()
     while True:
         now = time()
         if now - prev > 10:
-            # TODO: for loop every peer you know, do GET /getpeers requests
-            # client(ip, port)
+            with open('peers-' + sys.argv[1] + '.json', 'r') as f:
+                json_data = json.load(f)
+                for peer in json_data['peers']:
+                    ip,port = peer.split(':')
+                    client(ip, int(port))
             print("10 sec")
             prev = now
         else:
