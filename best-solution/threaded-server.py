46d0a90de589dbe008a20089282f6ba6db0dc324
karlmatti <karlmatti@hotmail.com>
2020-03-07 13:40:26 +0200

    fixed updating server list in peers-PORT.json

--- best-solution/threaded-server.py
+++ best-solution/threaded-server.py
@@ -12,7 +12,7 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
 
-        print("%s:%s sent:" % (self.client_address[0], self.client_address[1]))
+        #print("%s:%s sent:" % (self.client_address[0], self.client_address[1]))
 
         json_data = self.request.recv(1024).strip()
         data_list = json_data.decode().split('\n')
@@ -38,11 +38,11 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 def client(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-
+        print("/getpeers request to " + ip + str(port))
         sock.connect((ip, port))
         request = "GET /getpeers HTTP/1.1\r\nHost:%s\r\nUser-agent: client_4\r\n" % ip
         sock.send(request.encode())
-        print(request)
+        #print(request)
         # receive some data
 
         response = sock.recv(4096)
@@ -51,7 +51,7 @@ def client(ip, port):
         http_response = repr(response)
         http_response_len = len(http_response)
         # display the response
-        print("[RECV] - length: %d" % http_response_len)
+        #print("[RECV] - length: %d" % http_response_len)
 
 
         response_body = json.loads(http_response.split("\\r\\n")[-1][0:-1])
@@ -59,12 +59,11 @@ def client(ip, port):
 
         with open('peers-' + sys.argv[1] + '.json', 'r+') as f:  # loeme peers-PORT.json failist serverid sisse
             response_body = json.load(f)
-            new_peers = []
-            for peer in peers:
-                if peer not in response_body['peers']:
-                    new_peers.append(peer)
-        if response_body['peers']:
-            response_body['peers'] += new_peers
+        peers = set(peers + response_body['peers'])
+        response_body['peers'] = list(peers)
+
+        print("Peers:", list(peers))
+
         with open('peers-' + sys.argv[1] + '.json', 'w+') as f:  # kirjutame peers-PORT.json faili uuendatud andmed
             json.dump(response_body, f)
 
@@ -104,7 +103,7 @@ if __name__ == "__main__":
     # exit the server thread when the main thread terminates
     server_thread.daemon = True
     server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
+    #print("Server loop running in thread:", server_thread.name)
 
     # Reads default peers from peers-default.json file
     with open('peers-default.json', 'r') as f:
@@ -124,7 +123,7 @@ if __name__ == "__main__":
                 for peer in json_data['peers']:
                     ip, port = peer.split(':')
                     client(ip, int(port))
-            print("10 sec")
+            #print("10 sec")
             prev = now
         else:
             pass
